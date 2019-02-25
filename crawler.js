4cd37c893ce53a061424c994ffa9309b38eff3b4
lightyears1998 <lightyears1998@hotmail.com>
2019-02-26 01:07:03 +0800

    改善爬虫代码表现形式

--- crawler.js
+++ crawler.js
@@ -2,20 +2,31 @@
 
 
 const fs = require('fs');
+const path = require('path');
+const querystring = require('querystring');
 const request = require('request');
 const iconv = require('iconv-lite');
 const cheerio = require('cheerio');
 
 
 function crawl(title) {
+    let url_path = 'http://search.dangdang.com/';
+    let url_querystring = querystring.stringify({
+        key: title,
+        category_path: '01.00.00.00.00.00'  // 图书分类
+    })
+
+    console.log(`${url_path}?${url_querystring}`);
     request(
         {
-            uri:`http://search.dangdang.com/?key=${title}&category_path=01.00.00.00.00.00`,
+            uri: `${url_path}?${url_querystring}`,
             encoding: null
         },
         function (err, res, body) {
-            body = iconv.decode(body, 'gb2312').toString();
-            
+            if (!err) {
+                body = iconv.decode(body, 'gb2312').toString();
+                fs.writeFileSync(path.join(__dirname, 'cache', 'dangdang.html'), body);
+            }
         }
     );
 }
