8f6036fb5e5941480219bd1d0cf4f484edcb252c
lightyears1998 <lightyears1998@hotmail.com>
2019-02-26 15:59:45 +0800

    爬取任务的框架

--- crawler.js
+++ crawler.js
@@ -10,22 +10,28 @@ const cheerio = require('cheerio');
 
 
 function crawl(title) {
-    let url_path = 'http://search.dangdang.com/';
+    let url_path = 'http://search.dangdang.com/';  // 当当网搜索主页
     let url_querystring = querystring.stringify({
         key: title,
-        category_path: '01.00.00.00.00.00'  // 图书分类
+        category_path: '01.00.00.00.00.00'  // 指定仅在图书分类下搜索
     })
 
-    console.log(`${url_path}?${url_querystring}`);
+    let url = `${url_path}?${url_querystring}`;
     request(
         {
-            uri: `${url_path}?${url_querystring}`,
+            uri: url,
             encoding: null
         },
         function (err, res, body) {
             if (!err) {
-                body = iconv.decode(body, 'gb2312').toString();
-                fs.writeFileSync(path.join(__dirname, 'cache', 'dangdang.html'), body);
+                let charset = res.headers["content-type"].match(/charset=(.*)$/)[1];  // 获取网页编码
+                body = iconv.decode(body, charset).toString();  // 解码网页
+
+                const $ = cheerio.load(body);
+                $('div [dd_name=普通商品区域] ul').each((i, elem) => {
+                    
+                });
+                fs.writeFileSync(path.join(__dirname, 'cache', 'dangdang.html'), html.html());
             }
         }
     );
--- query.js
+++ query.js
@@ -4,23 +4,53 @@
 const path = require('path');
 const fs = require('fs');
 const crypto = require('crypto');
+const config = require('./config');
 const crawler = require('./crawler');
 
 
-function search_book(title) {
+// 通过标题搜索书籍，返回JSON搜索结果
+function query_by_title(title, sync) {
+    if (!title) {
+        return {
+            statusCode: -1,
+            statusDescription: '查询请求已拒绝，书名不能为空'
+        };
+    }
+
     let hash = crypto.createHash('sha1').update(title).digest('hex');
-    
-    return crawler(title);
+    let filepath = path.join(config.CACHE_PATH, 'query', `ST${hash}`);  // 保存任务状态的文件路径
+
+    // 判断此次查询是否正在进行
+    try {
+        payload = JSON.parse(fs.readFileSync(filepath).toString());
+    } 
+    catch(e) {
+        // 若文件不存在则开始爬取
+        if (sync) {
+            // 同步爬取，等所有爬取任务结束后合并结果再返回
+            // 未实现
+        }
+        else {
+            // 异步爬取
+            payload = {
+                statusCode: 1,
+                statusDescription: '查询请求已接受，正在爬取'
+            }
+            fs.writeFile(filepath, JSON.stringify(payload), (e) => {
+                console.log(e);
+            })
+        }
+    }
+
+    return payload;
 }
 
 
 function middleware(req, res) {
-    let title = req.query['title'];
-    
-    if (!title) {
-        res.send('必须通过书名查询');
-    }
-    res.send(search_book(title));
+    let sync = req.query['sync'];
+    let title = (req.query['title'] || '').trim();
+
+    res.json(query_by_title(title, sync));
 }
 
 
