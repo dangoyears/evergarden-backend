dee6c4c1350e7f1db6d23dfdd558f2f9ab2d3d75
lightyears <lightyears1998@hotmail.com>
2019-03-06 18:47:27 +0800

    计算并输出清理缓存文件的数量

--- toolkit.js
+++ toolkit.js
@@ -8,23 +8,30 @@ const config = require('./config');
 
 // 清理缓存目录中存在时间超过config.CACHE_EXPIRATION_MS中指定毫秒数的文件
 function cleanCache() {
-    fs.readdir(config.CACHE_PATH, (err, dirs) => {
-        dirs.forEach((dir => {
-            let dirpath = path.join(config.CACHE_PATH, dir);
-            fs.readdir(dirpath, (err, files) => {
-                if (!err) {
-                    files.forEach((file => {
-                        let filepath = path.join(dirpath, file);
-                        let modifiedDate = fs.lstatSync(filepath).mtime;
-                        
-                        if (modifiedDate.getTime() + config.CACHE_EXPIRATION_MS < new Date().getTime()) {
-                            fs.unlinkSync(filepath);
-                        }
-                    }));
+    let unlink_count = 0;
+
+    fs.readdirSync(config.CACHE_PATH).forEach((dir) => {
+        let dirpath = path.join(config.CACHE_PATH, dir);
+
+        try {
+            fs.readdirSync(dirpath).forEach((file => {
+                let filepath = path.join(dirpath, file);
+                let modifiedDate = fs.lstatSync(filepath).mtime;
+                
+                if (modifiedDate.getTime() + config.CACHE_EXPIRATION_MS < new Date().getTime()) {
+                    fs.unlinkSync(filepath);
+                    unlink_count++;
                 }
-            });
-        }));
+            }));
+        }
+        catch (err) {
+            if (err.code !== 'ENOTDIR') {
+                console.log(err);  // 打印除“指定路径不是目录”之外的错误
+            }
+        }
     });
+
+    console.log(`${new Date()}，清理的文件的数量：${unlink_count}`);
 }
 
 
