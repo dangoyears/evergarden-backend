f3f7dc1e857caa4519636f95a56d365ea556e93d
lightyears <lightyears1998@hotmail.com>
2019-03-07 19:03:47 +0800

    新增图像接口

--- image.js
+++ image.js
@@ -8,25 +8,17 @@ const config = require('./config');
 const crawlers = require('./crawler');
 
 
+// 通过URL爬取图片
 async function getImageByUrl(url)
 {
     let dangdang_pattern = /.*?ddimg.cn.*/;
     
     if (dangdang_pattern.test(url)) {
         let crawler = crawlers.DangdangCrawler.getImageByUrl;
-        let image = await crawler(url);
-        
-        console.log(image.info, image.raw.length);
+        return await crawler(url);
     }
 }
 
-
-// 检查指定hash的图像的完整性
-// 即检查指定图像是否存在、图像的元数据信息是否完整
-function existsImage(hash)
-{
-    return false;
-}
 
 async function middleware(req, res) {
     let image_base_path = path.join(config.CACHE_PATH, 'image');
@@ -35,22 +27,25 @@ async function middleware(req, res) {
     if (!image_url) {
         res.status(400);
         res.send('请求中必须包含url参数。');
+        return;
     }
 
-    // @todo 创建hash.raw和hash.info一遍对图像的元数据进行处理
-
-    let hash = crypto.createHash('sha1').update('image_url').digest('hex');
+    let hash = crypto.createHash('sha1').update(image_url).digest('hex');
     try {
         let image_info_path = path.resolve(path.join(image_base_path, hash + '.info'));
         let image_raw_path = path.resolve(path.join(image_base_path, hash + '.raw'));
-        if (fs.existsSync(image_info_path)) {
+        if (fs.existsSync(image_info_path) && fs.existsSync(image_raw_path)) {
+            let content_type = fs.readFileSync(image_info_path).toString();
+            res.setHeader('content-type', content_type);
             res.sendFile(image_raw_path);
         }
         else {
-            res.send(hash);
-            await getImageByUrl(image_url);
+            res.status(202);
+            res.send('正在爬取图片。');
+            let image = await getImageByUrl(image_url);
+            fs.writeFileSync(image_raw_path, image.raw);
+            fs.writeFileSync(image_info_path, image.info);
         }
-        console.log(image_info_path, image_raw_path);
     }
     catch (err) {
         console.log(err);
@@ -59,4 +54,4 @@ async function middleware(req, res) {
 
 module.exports = {
     middleware
-}
+};
